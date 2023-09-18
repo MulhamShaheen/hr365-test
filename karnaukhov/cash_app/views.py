a029dd1daf0994b272c36300f538b62601a0035e
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-09-18 12:45:10 +0300

    added bad request handle

--- karnaukhov/cash_app/views.py
+++ karnaukhov/cash_app/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework import status
 from .services import request_course
 
 
@@ -8,7 +8,10 @@ from .services import request_course
 def get_course(request):
     base_currency = request.GET['from']
     target = request.GET['to']
-    amount = request.GET['value']
+    amount = float(request.GET['value'])
+
+    if amount < 0:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     result = request_course(base_currency=base_currency, target=target, amount=float(amount))
 
